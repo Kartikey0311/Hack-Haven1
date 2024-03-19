6f0f0bda76c87bae3a6051275f153596eeaaec54
Kartikey0311 <139530094+Kartikey0311@users.noreply.github.com>
2024-03-19 22:05:02 +0530

    Add files via upload

---
+++ lana.js
@@ -0,0 +1,11 @@
+function callEmergency() {
+    window.location.href = 'tel:112'
+}
+
+function doctorEmergency(){
+    window.location.href = 'tel:108'
+}
+
+function policeEmergency(){
+    window.location.href = 'tel:100'
+}
